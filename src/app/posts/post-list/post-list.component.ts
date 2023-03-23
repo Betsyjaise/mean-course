47481de961a38ebe4bdf95a7616fbf259e73ebcc
Betsyjaise <betzybaby009@gmail.com>
2023-03-23 13:02:06 +0530

    subject and subscription

--- src/app/posts/post-list/post-list.component.ts
+++ src/app/posts/post-list/post-list.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
 import { Post } from '../post.model';
 import { PostsService } from '../posts.service';
 
@@ -7,12 +8,20 @@ import { PostsService } from '../posts.service';
   templateUrl: './post-list.component.html',
   styleUrls: ['./post-list.component.css']
 })
-export class PostListComponent implements OnInit {
+export class PostListComponent implements OnInit, OnDestroy {
   posts: Post[] = [];
-
+  private postsSub : Subscription = new Subscription;
   constructor(private postsService: PostsService) {}
 
   ngOnInit(): void {
     this.posts = this.postsService.getPosts();
+    this.postsSub= this.postsService.getPostUpdateListener()
+    .subscribe((posts:Post[])=>{
+      this.posts = posts;
+    });
+  }
+
+  ngOnDestroy() {
+    this.postsSub.unsubscribe();
   }
 }
