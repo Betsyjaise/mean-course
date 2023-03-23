77c9f4cf02727a3a859f933117b38648d6374ac3
Betsyjaise <betzybaby009@gmail.com>
2023-03-23 10:12:01 +0530

    data sharing

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -1,7 +1,7 @@
 import { Component, EventEmitter, Output } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 import { Post } from '../post.model';
-
 @Component({
   selector: 'app-post-create',
   templateUrl: './post-create.component.html',
@@ -11,10 +11,11 @@ export class PostCreateComponent {
   enteredContent = '';
   enteredTitle = '';
   @Output() postCreated = new EventEmitter<Post>();
-  onAddPost() {
+  onAddPost(form: NgForm) {
+    if(form.invalid)  return;
     const post: Post = {
-      title: this.enteredTitle,
-      content: this.enteredContent
+      title: form.value.title,
+      content: form.value.content
     };
     this.postCreated.emit(post);
   }
