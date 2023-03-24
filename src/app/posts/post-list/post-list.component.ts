3d7f31b71f3a5d2a4e12d3c60a056497e7b12a7d
Betsyjaise <betzybaby009@gmail.com>
2023-03-24 14:26:53 +0530

    style change

--- src/app/posts/post-list/post-list.component.ts
+++ src/app/posts/post-list/post-list.component.ts
@@ -6,19 +6,20 @@ import { PostsService } from '../posts.service';
 @Component({
   selector: 'app-post-list',
   templateUrl: './post-list.component.html',
-  styleUrls: ['./post-list.component.css']
+  styleUrls: ['./post-list.component.css'],
 })
 export class PostListComponent implements OnInit, OnDestroy {
   posts: Post[] = [];
-  private postsSub : Subscription = new Subscription;
+  private postsSub: Subscription = new Subscription();
   constructor(private postsService: PostsService) {}
 
   ngOnInit(): void {
     this.posts = this.postsService.getPosts();
-    this.postsSub= this.postsService.getPostUpdateListener()
-    .subscribe((posts:Post[])=>{
-      this.posts = posts;
-    });
+    this.postsSub = this.postsService
+      .getPostUpdateListener()
+      .subscribe((posts: Post[]) => {
+        this.posts = posts;
+      });
   }
 
   ngOnDestroy() {
